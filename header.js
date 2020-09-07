739e75cab7e68b5e6d126186c9b1553b9f8fb6ac
Mads Haerup <506094@rts-365.dk>
2020-09-07 17:43:06 +0200

    ripple effect, mode toggle, font-size

--- header.js
+++ header.js
@@ -1,13 +1,16 @@
 $(function() {
-  let menu = document.querySelector("#burgerMenu");
-  $("#burgerMenu").click(function() {
-    //Id #burger med funktion click
-    $("header").slideToggle(); //skal vise eller skjule nav ul når #burger bliver klikket
-    menu.classList.toggle("change"); //kalder classes der starter med change
+  let menu = document.querySelector("#menu");
+
+  $("#menu").click(function() {
+    $("header").slideToggle(); //show / hide
+    menu.classList.toggle("change"); //invoking class change
   });
 
-});
+  //selecting contact and invoking function ripples
+  $(contact).ripples();
 
+});
+// ────────────────────────────────────────────────────────────────────────────────
 let header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
@@ -20,4 +23,28 @@ window.addEventListener("scroll", () => {
     header.classList.remove("current");
   }
 
+});
+// Toggle between dark & light mode───────────────────────────────────────────────
+
+// Select the button
+const btn = document.querySelector("#checkbox");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+const label = document.querySelector(".check");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark-theme.css";
+    label.style.background = "#fff";
+
+    // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "light-theme.css";
+    label.style.background = "#20123a";
+
+  }
 });
